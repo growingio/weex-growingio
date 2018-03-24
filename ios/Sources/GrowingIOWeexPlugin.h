2f25b133e3000d76fbd4c40d303594f67d9ba3ee
nico <529725067@qq.com>
2018-03-24 19:44:00 +0800

    track code

---
+++ ios/Sources/GrowingIOWeexPlugin.h
@@ -0,0 +1,14 @@
+//
+//  GrowingIOWeexPlugin.h
+//  WeexGrowingIO
+//
+//  Created by apple on 2018/3/24.
+//  Copyright © 2018年 weexplugin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <WeexSDK/WeexSDK.h>
+
+@interface GrowingIOWeexPlugin : NSObject<WXModuleProtocol>
+
+@end
