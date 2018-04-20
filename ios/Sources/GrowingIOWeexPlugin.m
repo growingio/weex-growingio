79df523d198276b85c9fc0381fe5fe57e4310e5b
nico <529725067@qq.com>
2018-04-20 11:23:39 +0800

    delete callback

--- ios/Sources/GrowingIOWeexPlugin.m
+++ ios/Sources/GrowingIOWeexPlugin.m
@@ -16,13 +16,13 @@
 
 WX_PlUGIN_EXPORT_MODULE(GrowingIO, GrowingIOWeexPlugin)
 
-WX_EXPORT_METHOD(@selector(track:callback:))
-WX_EXPORT_METHOD(@selector(page:callback:))
-WX_EXPORT_METHOD(@selector(setPageVariable:pageLevelVariables:callback:))
-WX_EXPORT_METHOD(@selector(setEvar:callback:))
-WX_EXPORT_METHOD(@selector(setPeopleVariable:callback:))
-WX_EXPORT_METHOD(@selector(setUserId:callback:))
-WX_EXPORT_METHOD(@selector(clearUserId:))
+WX_EXPORT_METHOD(@selector(track:))
+WX_EXPORT_METHOD(@selector(page:))
+WX_EXPORT_METHOD(@selector(setPageVariable:pageLevelVariables:))
+WX_EXPORT_METHOD(@selector(setEvar:))
+WX_EXPORT_METHOD(@selector(setPeopleVariable:))
+WX_EXPORT_METHOD(@selector(setUserId:))
+WX_EXPORT_METHOD(@selector(clearUserId))
 
 NS_INLINE NSString *GROWGetTimestampFromTimeInterval(NSTimeInterval timeInterval) {
     return [NSNumber numberWithUnsignedLongLong:timeInterval * 1000.0].stringValue;
@@ -32,35 +32,27 @@ NS_INLINE NSString *GROWGetTimestamp() {
     return GROWGetTimestampFromTimeInterval([[NSDate date] timeIntervalSince1970]);
 }
 
-- (void)track:(NSDictionary *)event callback:(WXModuleKeepAliveCallback)callback
+- (void)track:(NSDictionary *)event
 {
     if (![event isKindOfClass:[NSDictionary class]]) {
-        if (callback) {
-            callback([self errorCallbackDictWithInfo:@"Argument error, The Argument event must be object type"], NO);
-        }
+        NSLog(@"Method(track) Argument error, The Argument event must be object type");
         return;
     }
     
     if (event.count == 0) {
-        if (callback) {
-            callback([self errorCallbackDictWithInfo:@"Argument error, The Argument event can not be empty"], NO);
-        }
+        NSLog(@"Method(track) Argument error, The Argument event can not be empty");
         return;
     }
     
     NSString *eventId = event[@"eventId"];
     
     if (![eventId isKindOfClass:[NSString class]]) {
-        if (callback) {
-            callback([self errorCallbackDictWithInfo:@"Argument error, The eventId value must be string type"], NO);
-        }
+        NSLog(@"Method(track) Argument error, The eventId value must be string type");
         return;
     }
     
     if (eventId.length == 0) {
-        if (callback) {
-            callback([self errorCallbackDictWithInfo:@"Argument error, The eventId value can not be empty"], NO);
-        }
+        NSLog(@"Method(track) Argument error, The eventId value can not be empty");
         return;
     }
     
@@ -74,15 +66,11 @@ NS_INLINE NSString *GROWGetTimestamp() {
         
     } else if (eventLevelVariable && number) {
         if (![eventLevelVariable isKindOfClass:[NSDictionary class]]) {
-            if (callback) {
-                callback([self errorCallbackDictWithInfo:@"Argument error, The eventLevelVariable value must be object type"], NO);
-            }
+            NSLog(@"Method(track) Argument error, The eventLevelVariable value must be object type");
             return;
         }
         if (![number isKindOfClass:[NSNumber class]]) {
-            if (callback) {
-                callback([self errorCallbackDictWithInfo:@"Argument error, The number value must be number type"], NO);
-            }
+            NSLog(@"Method(track) Argument error, The number value must be number type");
             return;
         }
         [self dispatchInMainThread:^{
@@ -91,9 +79,7 @@ NS_INLINE NSString *GROWGetTimestamp() {
         
     } else if (eventLevelVariable) {
         if (![eventLevelVariable isKindOfClass:[NSDictionary class]]) {
-            if (callback) {
-                callback([self errorCallbackDictWithInfo:@"Argument error, The eventLevelVariable value must be object type"], NO);
-            }
+            NSLog(@"Method(track) Argument error, The eventLevelVariable value must be object type");
             return;
         }
         [self dispatchInMainThread:^{
@@ -101,141 +87,97 @@ NS_INLINE NSString *GROWGetTimestamp() {
         }];
     } else if (number) {
         if (![number isKindOfClass:[NSNumber class]]) {
-            if (callback) {
-                callback([self errorCallbackDictWithInfo:@"Argument error, The number value must be number type"], NO);
-            }
+            NSLog(@"Method(track) Argument error, The number value must be number type");
             return;
         }
         [self dispatchInMainThread:^{
             [Growing track:eventId withNumber:number];
         }];
     }
-    
-    if (callback) {
-        callback([self successCallbackDictWithInfo:@"track"], NO);
-    }
 }
 
-- (void)page:(NSString *)page callback:(WXModuleKeepAliveCallback)callback
+- (void)page:(NSString *)page
 {
     if (![page isKindOfClass:[NSString class]]) {
-        if (callback) {
-            callback([self errorCallbackDictWithInfo:@"Argument error, The Argument page must be string type"], NO);
-        }
+        NSLog(@"Method(page) Argument error, The Argument page must be string type");
         return;
     }
     
     if (page.length == 0) {
-        if (callback) {
-            callback([self errorCallbackDictWithInfo:@"Argument error, The Argument page can not be empty"], NO);
-        }
+        NSLog(@"Method(page) Argument error, The Argument page can not be empty");
         return;
     }
     
     [self dispatchInMainThread:^{
         [Growing trackPageWithPageName:page pageTime:GROWGetTimestamp()];
     }];
-    
-    if (callback) {
-        callback([self successCallbackDictWithInfo:@"page"], NO);
-    }
 }
 
-- (void)setPageVariable:(NSString *)page pageLevelVariables:(NSDictionary *)pageLevelVariables callback:(WXModuleKeepAliveCallback)callback
+- (void)setPageVariable:(NSString *)page pageLevelVariables:(NSDictionary *)pageLevelVariables
 {
     if (![page isKindOfClass:[NSString class]]) {
-        if (callback) {
-            callback([self errorCallbackDictWithInfo:@"Argument error, The Argument page must be string type"], NO);
-        }
+        NSLog(@"Method(setPageVariable) Argument error, The Argument page must be string type");
         return;
     }
     
     if (![pageLevelVariables isKindOfClass:[NSDictionary class]]) {
-        if (callback) {
-            callback([self errorCallbackDictWithInfo:@"Argument error, The Argument pageLevelVariables must be string type"], NO);
-        }
+        NSLog(@"Method(setPageVariable) Argument error, The Argument pageLevelVariables must be objct type");
         return;
     }
     
     if (page.length == 0 || page.length > 1000) {
-        if (callback) {
-            callback([self errorCallbackDictWithInfo:@"Argument error, The Argument page length can not > 1000 or = 0"], NO);
-        }
+        NSLog(@"Method(setPageVariable) Argument error, The Argument page length can not > 1000 or = 0");
         return;
     }
     
     if (pageLevelVariables.count == 0) {
-        if (callback) {
-            callback([self errorCallbackDictWithInfo:@"Argument error, The Argument pageLevelVariables can not be empty"], NO);
-        }
+        NSLog(@"Method(setPageVariable) Argument error, The Argument pageLevelVariables can not be empty");
         return;
     }
     
     [self dispatchInMainThread:^{
         [Growing setPageVariable:pageLevelVariables toPage:page];
     }];
-    
-    if (callback) {
-        callback([self successCallbackDictWithInfo:@"setPageVariable"], NO);
-    }
 }
 
-- (void)setEvar:(NSDictionary *)conversionVariables callback:(WXModuleKeepAliveCallback)callback
+- (void)setEvar:(NSDictionary *)conversionVariables
 {
     if (![conversionVariables isKindOfClass:[NSDictionary class]]) {
-        if (callback) {
-            callback([self errorCallbackDictWithInfo:@"Argument error, The Argument conversionVariables must be object type"], NO);
-        }
+        NSLog(@"Method(setEvar) Argument error, The Argument conversionVariables must be object type");
         return;
     }
     
     if (conversionVariables.count == 0) {
-        if (callback) {
-            callback([self errorCallbackDictWithInfo:@"Argument error, The Argument conversionVariables can not be empty"], NO);
-        }
+        NSLog(@"Method(setEvar) Argument error, The Argument conversionVariables can not be empty");
         return;
     }
     
     [self dispatchInMainThread:^{
         [Growing setEvar:conversionVariables];
     }];
-    
-    if (callback) {
-        callback([self successCallbackDictWithInfo:@"setEvar"], NO);
-    }
 }
 
-- (void)setPeopleVariable:(NSDictionary *)peopleVariables callback:(WXModuleKeepAliveCallback)callback
+- (void)setPeopleVariable:(NSDictionary *)peopleVariables
 {
     if (![peopleVariables isKindOfClass:[NSDictionary class]]) {
-        if (callback) {
-            callback([self errorCallbackDictWithInfo:@"Argument error, The Argument peopleVariables must be object type"], NO);
-        }
+        NSLog(@"Method(setPeopleVariable) Argument error, The Argument peopleVariables must be object type");
         return;
     }
     
     if (peopleVariables.count == 0) {
-        if (callback) {
-            callback([self errorCallbackDictWithInfo:@"Argument error, The Argument peopleVariables can not be empty"], NO);
-        }
+        NSLog(@"Method(setPeopleVariable) Argument error, The Argument peopleVariables can not be empty");
         return;
     }
     
     [self dispatchInMainThread:^{
         [Growing setPeopleVariable:peopleVariables];
     }];
-    
-    if (callback) {
-        callback([self successCallbackDictWithInfo:@"setPeopleVariable"], NO);
-    }
 }
 
-- (void)setUserId:(NSString *)userId callback:(WXModuleKeepAliveCallback)callback
+- (void)setUserId:(NSString *)userId
 {
     if (![userId isKindOfClass:[NSString class]] && ![userId isKindOfClass:[NSNumber class]]) {
-        if (callback) {
-            callback([self errorCallbackDictWithInfo:@"Argument error, The Argument userId must be string type"], NO);
-        }
+        NSLog(@"Method(setUserId) Argument error, The Argument userId must be string type");
         return;
     }
     
@@ -244,43 +186,22 @@ NS_INLINE NSString *GROWGetTimestamp() {
     }
     
     if (userId.length == 0 || userId.length > 1000) {
-        if (callback) {
-            callback([self errorCallbackDictWithInfo:@"Argument error, The Argument userId length can not > 1000 or = 0"], NO);
-        }
+        NSLog(@"Method(setUserId) Argument error, The Argument userId length can not > 1000 or = 0");
         return;
     }
     
     [self dispatchInMainThread:^{
         [Growing setPluginUserId:userId];
     }];
-    
-    if (callback) {
-        callback([self successCallbackDictWithInfo:@"setUserId"], NO);
-    }
 }
 
-- (void)clearUserId:(WXModuleKeepAliveCallback)callback
+- (void)clearUserId
 {
     [self dispatchInMainThread:^{
         [Growing clearPluginUserId];
     }];
-    
-    if (callback) {
-        callback([self successCallbackDictWithInfo:@"clearUserId"], NO);
-    }
 }
 
-- (NSMutableDictionary *)successCallbackDictWithInfo:(NSString *)info
-{
-    NSMutableDictionary *callbackDict = [NSMutableDictionary dictionaryWithObjectsAndKeys:[NSString stringWithFormat:@"%@ success", info], @"info", @"success", @"result", nil];
-    return callbackDict;
-}
-
-- (NSMutableDictionary *)errorCallbackDictWithInfo:(NSString *)info
-{
-    NSMutableDictionary *callbackDict = [NSMutableDictionary dictionaryWithObjectsAndKeys:info, @"info", @"error", @"result", nil];
-    return callbackDict;
-}
 
 - (void)dispatchInMainThread:(void (^)(void))block
 {
